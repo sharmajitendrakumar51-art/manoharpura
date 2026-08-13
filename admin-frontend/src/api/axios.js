e0b1727d239e1007a6707372ca9a666685972a00
Sachin Sharma <SachinSharma@123.com>
2026-08-13 13:15:22 +0530

    Updated frontend and backend

--- admin-frontend/src/api/axios.js
+++ admin-frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://manoharpura.onrender.com/api",
-  baseURL: "http://localhost:3000/api"
+   baseURL: "https://manoharpura.onrender.com/api",
+ // baseURL: "http://localhost:3000/api"
 });
 
 // ==========================
